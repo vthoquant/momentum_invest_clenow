3afa467e0bca499442fba991e82ca4c477f3c2b9
vthoquant <38273791+vthoquant@users.noreply.github.com>
2021-09-07 10:49:43 +0530

    batch file to run portoflio rebalancer immediately after generating the portfolio spreadsheet for that day

--- batch/daily_portfolio_analysis.bat
+++ batch/daily_portfolio_analysis.bat
@@ -5,4 +5,8 @@ echo =======================================================
 echo running daily clenow-style portfolio analysis
 echo =======================================================
 python runner.py
+echo =======================================================
+echo computing diffs against previous run-date
+echo =======================================================
+python portfolio_rebalance.py
 conda deactivate
